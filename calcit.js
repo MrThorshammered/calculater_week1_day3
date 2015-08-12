9bb999def17a49409dc9a5094aa58a7c6b47f264
Mike Jones <mikethespike77@gmail.com>
2015-08-12 17:30:51 +0100

    first potential layout

--- calcit.js
+++ calcit.js
@@ -2,9 +2,16 @@
 
 console.log('op', op);
 
- var num = prompt('choose a number');
- console.log('num', num);
+ var numone = prompt('Choose your first number!');
+ console.log('num1', num);
+
+ var numtwo = prompt('Choose your second number!');
+ console.log('num2', num);
 
  var goodNum = parseFloat(num);
+
+ var answer = function('num1' + 'op' + 'num2');
+
+ alert("your answer is "+ answer)
 
  alert("Why is your console not open?");
