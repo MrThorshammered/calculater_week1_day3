fab51f10d73898e8c06cd37b2e2d181ce5092d49
Mike Jones <mikethespike77@gmail.com>
2015-08-12 18:15:12 +0100

    starting work on advanced calc

--- calcit.js
+++ calcit.js
@@ -1,3 +1,7 @@
+ var calcType = prompt('Would you like a simple or advanced calculator?');
+
+ if (calcType === 'simple'){
+
  var num1 = parseInt(prompt('Choose your first number!'));
  console.log('num1', num1);
 
@@ -20,5 +24,36 @@ if (op === 'plus'){
  };
 
  alert("your answer is "+ answer)
+
+}else if (calcType === 'advanced') {
+
+var num1 = parseInt(prompt('Choose your first number!'));
+ console.log('num1', num1);
+
+ var op = prompt('Choose an operation plus, minus, divide, times');
+ console.log('op', op);
+
+ var num2 = parseInt(prompt('Choose your second number!'));
+ console.log('num2', num2);
+
+if (op === 'plus'){
+ var answer = (num1) + (num2);
+ }else if (op === 'minus'){
+ 	var answer = (num1) - (num2);
+ }else if (op === 'divide') {
+ 	var answer = (num1) / (num2);
+ }else if (op === 'times') {
+ 	var answer = (num1) * (num2);
+ }else{
+ 	alert("error")
+ };
+
+ alert("your answer is "+ answer)
+
+}else{
+	alert("error")
+};
+
+
 
  // alert("Why is your console not open?");
