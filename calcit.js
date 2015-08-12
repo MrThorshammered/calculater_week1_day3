557def7faec99bc554f8519ba0d650b03a92e060
Mike Jones <mikethespike77@gmail.com>
2015-08-12 17:04:37 +0100

    initial file structure

---
+++ calcit.js
@@ -0,0 +1,10 @@
+ var op = prompt('Choose an operation');
+
+console.log('op', op);
+
+ var num = prompt('choose a number');
+ console.log('num', num);
+
+ var goodNum = parseFloat(num);
+
+ alert("Why is your console not open?");
