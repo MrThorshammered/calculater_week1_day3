b553cca6ca695e57746fc4ffee8b3fa34fdb3749
Mike Jones <mikethespike77@gmail.com>
2015-08-12 18:05:35 +0100

    got the basic calc going

--- calcit.js
+++ calcit.js
@@ -1,17 +1,24 @@
- var op = prompt('Choose an operation');
+ var num1 = parseInt(prompt('Choose your first number!'));
+ console.log('num1', num1);
 
-console.log('op', op);
+ var op = prompt('Choose an operation plus, minus, divide, times');
+ console.log('op', op);
 
- var numone = prompt('Choose your first number!');
- console.log('num1', num);
+ var num2 = parseInt(prompt('Choose your second number!'));
+ console.log('num2', num2);
 
- var numtwo = prompt('Choose your second number!');
- console.log('num2', num);
-
- var goodNum = parseFloat(num);
-
- var answer = function('num1' + 'op' + 'num2');
+if (op === 'plus'){
+ var answer = (num1) + (num2);
+ }else if (op === 'minus'){
+ 	var answer = (num1) - (num2);
+ }else if (op === 'divide') {
+ 	var answer = (num1) / (num2);
+ }else if (op === 'times') {
+ 	var answer = (num1) * (num2);
+ }else{
+ 	alert("error")
+ };
 
  alert("your answer is "+ answer)
 
- alert("Why is your console not open?");
+ // alert("Why is your console not open?");
