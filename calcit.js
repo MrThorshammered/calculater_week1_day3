dc32b7a57171aabc0c7fb3664b3461d21c9fa643
Mike Jones <mikethespike77@gmail.com>
2015-08-12 19:04:49 +0100

    got the advanced to work with power and square root

--- calcit.js
+++ calcit.js
@@ -33,7 +33,7 @@ var num1 = parseInt(prompt('Choose your first number!'));
  var op = prompt('Choose an operation plus, minus, divide, times, power, square root');
  console.log('op', op);
 
-if (!op === 'square root'){
+if (op !== 'square root'){
  var num2 = parseInt(prompt('Choose your second number!'));
  console.log('num2', num2)};
 
@@ -46,9 +46,9 @@ if (op === 'plus'){
  }else if (op === 'times') {
  	var answer = (num1) * (num2);
  }else if (op === 'power') {
- 	var answer = (num1) * (num2);
+ 	var answer = Math.pow(num1, num2);
  }else if (op === 'square root') {
- 	var answer = math.sqrt(num1);
+ 	var answer = Math.sqrt(num1);
  }else{
  	alert("error")
  };
